f16ab319778b83e36a192b64bf18c8ecc80216ca
Ivan Jelisavcic <ivanjelisavcic6@gmail.com>
2024-02-14 15:53:51 +0100

    Css za Login stranicu

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -33,76 +33,40 @@ function LoginPage({ addToken, addUser }) {
     });
   }
   return (
-    <section
-      className="vh-100"
-      style={{
-        paddingTop: 4.5 + "rem",
-      }}
-    >
-      <div className="container-fluid h-custom">
-        <div className="row d-flex justify-content-center align-items-center h-100">
-          <div className="col-md-9 col-lg-6 col-xl-5">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-              className="img-fluid"
-              alt="Sample image"
-            />
-          </div>
-          <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-            <form onSubmit={handleLogin}>
-              <div className="form-outline mb-4">
-                <input
-                  type="email"
-                  id="form3Example3"
-                  className="form-control form-control-lg"
-                  placeholder="Enter a valid email address"
-                  name="email"
-                  onInput={handleInput}
-                />
-                <label className="form-label" htmlFor="form3Example3">
-                  Email address
-                </label>
-              </div>
-
-              <div className="form-outline mb-3">
-                <input
-                  type="password"
-                  id="form3Example4"
-                  className="form-control form-control-lg"
-                  placeholder="Enter password"
-                  name="password"
-                  onInput={handleInput}
-                />
-                <label className="form-label" htmlFor="form3Example4">
-                  Password
-                </label>
-              </div>
-
-              <div className="text-center text-lg-start mt-4 pt-2">
-                <button
-                  type="submit"
-                  className="btn btn-primary btn-lg"
-                  style={{
-                    paddingLeft: 2.5 + "rem",
-                    paddingRight: 2.5 + "rem",
-                  }}
-                >
-                  Login
-                </button>
-                <p className="small fw-bold mt-2 pt-1 mb-0">
-                  Don't have an account?{" "}
-                  <a href="/register" className="link-danger">
-                    Register
-                  </a>
-                </p>
-              </div>
-            </form>
-          </div>
+    <section className="login-page">
+      <div className="login-container">
+        <div className="login-form">
+          <h2 className="login-title">Login</h2>
+          <form onSubmit={handleLogin}>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control input-field"
+                placeholder="Enter your email"
+                name="email"
+                onInput={handleInput}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control input-field"
+                placeholder="Enter your password"
+                name="password"
+                onInput={handleInput}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary btn-login">
+              Login
+            </button>
+          </form>
+          <p className="register-link">
+            Don't have an account? <a href="/register">Register</a>
+          </p>
         </div>
       </div>
     </section>
-
-  )
+  );
 }
 
 export default LoginPage
