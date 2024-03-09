80b59953340ace5679849ee375a408f4d0091af9
Ivan Jelisavcic <ivanjelisavcic6@gmail.com>
2024-03-09 17:16:07 +0100

    MyAuctionsPage prikaz zavrrsenih aukcija(uspesnih i neuspesnih)

--- src/MyAuctionsPage.jsx
+++ src/MyAuctionsPage.jsx
@@ -1,48 +1,140 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { BsCheckCircleFill } from "react-icons/bs";
+import { VscError } from "react-icons/vsc";
 
-function MyAuctionsPage({token}) {
+
+function MyAuctionsPage({ token }) {
   const [auctions, setAuctions] = useState([]);
+  const [successfulPurchases, setSuccessfulPurchases] = useState([]);
+  const [unsuccessfulPurchases, setUnsuccessfulPurchases] = useState([]);
+
+  const getBuyer = async (buyer_id) => {
+    try {
+      const response = await axios.get(`api/users/${buyer_id}`, {
+        headers: {
+          'Authorization': 'Bearer ' + token
+        }
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error getting buyer:', error);
+      return null;
+    }
+  }
 
   useEffect(() => {
-    let config = {
-      method: 'get',
-      maxBodyLength: Infinity,
-      url: 'api/myauctions',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' +token
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('api/myauctions', {
+          headers: {
+            'Authorization': 'Bearer ' + token
+          }
+        });
+        setAuctions(response.data);
+      } catch (error) {
+        console.error('Error fetching auctions:', error);
       }
     };
+    fetchData();
+  }, [token]);
 
-    axios.request(config)
-    .then((response) => {
-      console.log(response);
-      setAuctions(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('api/purchasesSuccessful', {
+          headers: {
+            'Authorization': 'Bearer ' + token
+          }
+        });
+        const data = await Promise.all(response.data.map(async (purchase) => {
+          const buyer = await getBuyer(purchase.buyer_id);
+          return {
+            ...purchase,
+            buyer_name: buyer ? buyer.name : '',
+            buyer_phone: buyer ? buyer.phone_number : ''
+          };
+        }));
+        setSuccessfulPurchases(data);
+      } catch (error) {
+        console.error('Error fetching successful purchases:', error);
+      }
+    };
+    fetchData();
+  }, [token]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('api/purchasesUnsuccessful', {
+          headers: {
+            'Authorization': 'Bearer ' + token
+          }
+        });
+        setUnsuccessfulPurchases(response.data);
+      } catch (error) {
+        console.error('Error fetching unsuccessful purchases:', error);
+      }
+    };
+    fetchData();
+  }, [token]);
+
   return (
+    <div id="myAuctionsPage">
     <div className="myauctions-page-container">
-      <h2 className="myauctions-page-title">My Auctions</h2>
+      <h1 className="myauctions-page-title">My Ongoing Auctions</h1>
       <div className="myauctions-list">
-        {auctions.map(auction => (
-          <div key={auction.id} className="myauction-card">
-            {auction.image_path && (
-              <img src={auction.image_path} alt={auction.product_name} className="myauction-image" />
-            )}
-            <div className="myauction-details">
-              <h3 className="myauction-title">{auction.product_name}</h3>
-              <p className="myauction-description">{auction.description}</p>
-              <p className="myauction-price">Current Price: ${auction.current_price}</p>
-              <p className="myauction-start">Starts: {new Date(auction.start).toLocaleString()}</p>
-              <p className="myauction-end">Ends: {new Date(auction.end).toLocaleString()}</p>
-            </div>
-          </div>
-        ))}
+        {/* Rendering auctions */}
       </div>
+
+      <div className="table-container">
+        <h2 className="table-title">Successful Auctions</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th className="table-header">Product</th>
+              <th className="table-header">Auction ID</th>
+              <th className="table-header">Buyer</th>
+              <th className="table-header">Phone Number</th>
+              <th className="table-header">Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {successfulPurchases.map(purchase => (
+              <tr key={purchase.id}>
+                <td className="table-data product-column"><BsCheckCircleFill className="success-icon" />{purchase.product_name}</td>
+                <td className="table-data">{purchase.auction_id}</td>
+                <td className="table-data">{purchase.buyer_name}</td>
+                <td className="table-data">{purchase.buyer_phone}</td>
+                <td className="table-data">${purchase.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="table-container">
+        <h2 className="table-title">Unsuccessful Auctions</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th className="table-header">Product</th>
+              <th className="table-header">Auction ID</th>
+              <th className="table-header">Price</th>
+            </tr>
+          </thead>
+          <tbody>
+          {unsuccessfulPurchases.map(purchase => (
+              <tr key={purchase.id}>
+                <td className="table-data product-column"><VscError className="error-icon" />{purchase.product_name}</td>
+                <td className="table-data">{purchase.auction_id}</td>
+                <td className="table-data">${purchase.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
     </div>
   );
 }
