4c60e18aa90911158a8f20c6f49cc52cc2e94e77
Ivan Jelisavcic <ivanjelisavcic6@gmail.com>
2024-07-10 14:09:14 +0200

    Male izmene

--- src/Auctions.jsx
+++ src/Auctions.jsx
@@ -3,14 +3,15 @@ import OneAuction from './OneAuction';
 import './App.css';
 import axios from 'axios';
 
-function Auctions({ auctions, categories,logedUser }) {
+function Auctions({ auctions, categories, logedUser }) {
     const [selectedCategory, setSelectedCategory] = useState(null);
     const [expiredAuctions, setExpiredAuctions] = useState([]);
     const [activeAuctions, setActiveAuctions] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     // Funkcija za filtriranje aukcija po odabranoj kategoriji
     const filterAuctionsByCategory = (categoryId) => {
-      setSelectedCategory(categoryId === selectedCategory ? null : categoryId);
+        setSelectedCategory(categoryId === selectedCategory ? null : categoryId);
     };
 
     // Funkcija za brisanje aukcije
@@ -48,47 +49,63 @@ function Auctions({ auctions, categories,logedUser }) {
         return () => clearInterval(interval); // Čistimo interval kada se komponenta demontira
     }, [auctions]);
 
+    // Funkcija za filtriranje aukcija po nazivu proizvoda
+    const filterAuctionsByName = (auctions, searchTerm) => {
+        return auctions.filter(auction => auction.product_name.toLowerCase().includes(searchTerm.toLowerCase()));
+    };
+
+    const filteredActiveAuctions = filterAuctionsByName(activeAuctions, searchTerm)
+        .filter(auction => !selectedCategory || auction.category_id === selectedCategory);
+    const filteredExpiredAuctions = filterAuctionsByName(expiredAuctions, searchTerm)
+        .filter(auction => !selectedCategory || auction.category_id === selectedCategory);
+
     return (
-      <div className="auctionsPage">
-        <div className='auctionsPageTitle'>
-          <h3>These are all auctions from the database!</h3>
-        </div>
-        <div className="categoryButtons">
-          {categories && categories.length > 0 && categories.map(category => (
-            <button
-              key={category.id}
-              className={selectedCategory === category.id ? 'activeCategory' : ''}
-              onClick={() => filterAuctionsByCategory(category.id)}
-            >
-              {category.category_name}
-            </button>
-          ))}
-        </div>
-        <h1>Active Auctions</h1>
-        <div className="auctionsPageContent">
-          {activeAuctions == null || activeAuctions.length === 0 ? (
-            <p>No active auctions</p>
-          ) : (
-            activeAuctions
-            .filter(auction => !selectedCategory || auction.category_id === selectedCategory)
-            .map(auction => (
-                <div key={auction.id}>
-                    <OneAuction auction={auction} logedUser={logedUser} isActive={true} />
-                </div>
-            ))
-          )}
-        </div>
-        <h1>Expired Auctions</h1>
-        <div className="expiredAuctions">
-
-          {expiredAuctions.length > 0 ? expiredAuctions.map(auction => (
-            <div key={auction.id}>
-              <OneAuction auction={auction} logedUser={logedUser} isActive={false} />
+        <div className="auctionsPage">
+            <div className='auctionsPageTitle'>
+                <h3>These are all auctions from the database!</h3>
             </div>
-          )) : <p>No expired auctions</p>}
+            <div className="searchBar">
+                <input
+                    className='searchBarInput'
+                    type="text"
+                    placeholder="Search by product name..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+            <div className="categoryButtons">
+                {categories && categories.length > 0 && categories.map(category => (
+                    <button
+                        key={category.id}
+                        className={selectedCategory === category.id ? 'activeCategory' : ''}
+                        onClick={() => filterAuctionsByCategory(category.id)}
+                    >
+                        {category.category_name}
+                    </button>
+                ))}
+            </div>
+            <h1>Active Auctions</h1>
+            <div className="auctionsPageContent">
+                {filteredActiveAuctions == null || filteredActiveAuctions.length === 0 ? (
+                    <p>No active auctions</p>
+                ) : (
+                    filteredActiveAuctions.map(auction => (
+                        <div key={auction.id}>
+                            <OneAuction auction={auction} logedUser={logedUser} isActive={true} />
+                        </div>
+                    ))
+                )}
+            </div>
+            <h1>Expired Auctions</h1>
+            <div className="expiredAuctions">
+                {filteredExpiredAuctions.length > 0 ? filteredExpiredAuctions.map(auction => (
+                    <div key={auction.id}>
+                        <OneAuction auction={auction} logedUser={logedUser} isActive={false} />
+                    </div>
+                )) : <p>No expired auctions</p>}
+            </div>
         </div>
-      </div>
     );
-  }
+}
 
-  export default Auctions;
+export default Auctions;
